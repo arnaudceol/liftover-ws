4a9689efd24e202b1de3b1c73da7bb2bec4f0c6b
Arnaud Ceol <arnaud.ceol.pro@gmail.com>
2014-06-12 15:28:04 +0200

    fix hard coded position

--- liftover-client/src/main/java/it/iit/genomics/cru/bridges/liftover/client/LiftOverClient.java
+++ liftover-client/src/main/java/it/iit/genomics/cru/bridges/liftover/client/LiftOverClient.java
@@ -19,6 +19,8 @@ import it.iit.genomics.cru.bridges.liftover.ws.LiftOver;
 import it.iit.genomics.cru.bridges.liftover.ws.LiftOverService;
 import it.iit.genomics.cru.bridges.liftover.ws.Mapping;
 
+import javax.xml.ws.BindingProvider;
+
 /**
  * 
  * @author Arnaud Ceol arnaud.ceol@iit.it
@@ -29,22 +31,28 @@ public class LiftOverClient {
 	LiftOver port;
 	
 	public LiftOverClient() {
-		LiftOverService service = new LiftOverService();
-	    port = service.getLiftOverPort();  
+		LiftOverService service = new LiftOverService();		
+	    port = service.getLiftOverPort();	    
 	}
+	
+	public LiftOverClient(String endPointURL) {
+		LiftOverService service = new LiftOverService();		
+	    port = service.getLiftOverPort();
+	    ((BindingProvider)port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endPointURL);	    
+	}
+	
 	
 	public Mapping getMapping(String genome, String fromAssembly,
 			String toAssembly, String chromosome, int start, int end) {
-		return port.getMapping("Human", "hg18", "hg19"
-				, "chr1",    743267,  743268);
+		return port.getMapping(genome, fromAssembly, toAssembly
+				, chromosome,    start,  end);
 	}
 	
 	public static void main(String[] args) throws Exception{
-	
 		LiftOverClient client = new LiftOverClient();  
-        
-	    Mapping mapping = client.getMapping("Human", "hg18", "hg19"
-				, "chr1",    743267,  743268);
+
+		Mapping mapping = client.getMapping("Human", "hg19", "hg38"
+				, "chr7",    150648737,  150648738);
 	    
 	    System.out.println("lift over: "+mapping.getChromosome() + " " + mapping.getStart() + " " + mapping.getEnd());
         
